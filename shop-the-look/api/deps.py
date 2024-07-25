921198c90f26e0e41c37789dc4e5d208e6b1a7d0
Jacky Liang <jackyliang@users.noreply.github.com>
2024-07-25 13:32:58 -0400

    added sourcing

--- shop-the-look/api/deps.py
+++ shop-the-look/api/deps.py
@@ -1,5 +1,5 @@
 from pinecone import Pinecone
 from api.config import settings
 
-pc = Pinecone(api_key=settings.api_key)
+pc = Pinecone(api_key=settings.api_key, source_tag="pinecone:stl_sample_app")
 index = pc.Index(settings.index_name)
